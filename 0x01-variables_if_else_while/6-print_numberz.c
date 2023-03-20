3e57c7238288d9d24718fdab59fcd952c68c612c
root <shroukouda2@gmail.com>
2023-03-20 08:15:41 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description:print nums from 0 to 9 with for
+ *
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	putchar(ch);
+	putchar('\n');
+	return (0);
+}
