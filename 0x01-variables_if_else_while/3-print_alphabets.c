f51815c7bbabf0dadb9ec43745497c2f45d75e58
root <shroukouda2@gmail.com>
2023-03-20 06:19:47 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,13 +9,11 @@
 int main(void)
 {
 	char SH, CH;
-	char D = '$';
 
 	for (SH = 'a'; SH <= 'z'; SH++)
 		putchar(SH);
 	for (CH = 'A'; CH <= 'Z'; CH++)
 		putchar(CH);
-	putchar(D);
 	putchar('\n');
 	return (0);
 }
