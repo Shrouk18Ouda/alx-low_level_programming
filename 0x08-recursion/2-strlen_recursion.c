29738de2c9bcb8179598bcc5be17e946967b75b4
root <shroukouda2@gmail.com>
2023-04-09 17:22:59 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,11 +4,15 @@
  *
  * @s : char
  *
- * Return: 0
+ * Return: name
 */
 int _strlen_recursion(char *s)
 {
-	putchar(*s);
-	putchar('\n');
-	_strlen_recursion(s);
+	int name = 0;
+
+	if (*s > '\0')
+	{
+	name = name + _strlen_recursion(s + 1) + 1;
+	}
+return (name);
 }
